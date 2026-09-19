bb9353b2061b3175eb5dd4974717f720677c36e8
rajbhadrannavar1 <rajbhadrannavar17@gmail.com>
2026-09-19 23:31:19 +0530

    fix Vercel API serverless deployment

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,6 +1,9 @@
 import { app } from '../apps/api/src/server.js';
 
+let ready: Promise<void> | undefined;
+
 export default async function handler(req: any, res: any) {
-  await app.ready();
+  ready ??= app.ready();
+  await ready;
   app.server.emit('request', req, res);
 }
